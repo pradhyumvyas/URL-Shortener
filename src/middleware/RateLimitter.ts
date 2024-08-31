7c543c226649cb406fe1dd7bd3d374e17e37670f
Pradhyum Vyas <pradhyumvyas92@gmail.com>
2024-08-31 21:21:29 +0530

    Added Rate Limiting

--- src/middleware/RateLimitter.ts
+++ src/middleware/RateLimitter.ts
@@ -2,7 +2,7 @@ const setRateLimit = require("express-rate-limit");
 
 // Rate limit middleware
 const rateLimitMiddleware = setRateLimit({
-  windowMs: 60 * 1000,
+  windowMs: 15 * 60 * 1000, //For 15 minutes
   max: 5,
   message: "You have exceeded your 5 requests per minute limit.",
   headers: true,
